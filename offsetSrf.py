11765d6969567dc004d38b5a7bd1afd33b9432dc
kstolzenberg <kstolzenberg@gmail.com>
2016-02-21 19:27:10 -0500

    + works but no trim :/

--- offsetSrf.py
+++ offsetSrf.py
@@ -2,34 +2,25 @@ import rhinoscriptsyntax as rs
 
 polySrfs = rs.GetObjects("pick polysrfs")
 
-fourSrfs = [] # this is a list of lists of each polysurface shape
-allSrfs = [] # not sure you need this the other one is already an arrray!
+AllSrfs = [] # this is a list of lists of each polysurface shape
 thickSrfs = []
 
 # need a structure of a gloabl list of arrays of 4 srfs
 # the answer is always needz more data structure
 
 for obj in polySrfs:
-    fourSrfs.append(rs.ExplodePolysurfaces(obj,True))
+    AllSrfs.append(rs.ExplodePolysurfaces(obj,True))
 
-print fourSrfs
-
-for srf in fourSrfs[0]:
-    thick = rs.OffsetSurface(srf,-1,None,False,True)
-    rs.DeleteObject(srf)
-    thickSrfs.append(thick)
-
-solid = rs.BooleanUnion(thickSrfs,True)
+for shape in AllSrfs:
+    thick_shapes = []
+    for srf in shape:
+        thick = rs.OffsetSurface(srf,-.5,None,False,True)
+        rs.DeleteObject(srf)
+        thick_shapes.append(thick)
+    
+    print rs.BooleanUnion(thick_shapes)
 
 # need to add merge face support - something in Rhino Common? 
 # Rhino.Geometry.Brep.MergeCoplanarFaces
-
-#for srf in singleSrfs:
-#    thick = rs.OffsetSurface(srf,-1,None,False, True)
-#    rs.DeleteObject(srf)
-#    thickSrfs.append(thick) 
-
-# this boolean fails because its try things that don't touch - you need a data structure for each quad
-#solids = rs.BooleanUnion(thickSrfs,True)
 
 print "done"
